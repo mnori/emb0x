4b44fec4a6fb66ab9024f5d4ef65db602d18e9ee
Matthew Norris <matthew.gs.norris@gmail.com>
2025-03-30 03:59:34 +0100

    Improved schema

--- bin/init-database.sh
+++ bin/init-database.sh
@@ -9,3 +9,5 @@ SQL_FILE="init-database.sql"
 
 # Execute the SQL file inside the MySQL container
 docker exec -i $CONTAINER_NAME mysql -u$DB_USER -p$DB_PASSWORD $DB_NAME < $SQL_FILE
+
+echo "Reached the end of initialising the database"
--- bin/init-database.sql
+++ bin/init-database.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS track;
+DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS track_artist;
-DROP TABLE IF EXISTS `release`;
-DROP TABLE IF EXISTS release_track;
-DROP TABLE IF EXISTS release_artist;
+DROP TABLE IF EXISTS release_unit;
+DROP TABLE IF EXISTS release_unit_track;
+DROP TABLE IF EXISTS release_unit_artist;
 
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT,
@@ -21,24 +21,27 @@ CREATE TABLE user (
 CREATE TABLE track (
     id INT NOT NULL AUTO_INCREMENT,
     track_name VARCHAR(255) NULL,
-    artist_name VARCHAR(255) NULL,
+    artist_name VARCHAR(255) NULL, -- for when the artist is yet to be a user
     artist_user_id INT NULL, -- FK to User table
     track_description TEXT NULL,
     genre VARCHAR(255) NULL, -- FK to Genre table
     created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_on TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     removed_on TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user_id FOREIGN KEY (artist_user_id) REFERENCES user(id),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE track_artist (
     track_id INT NOT NULL,
     artist_id INT NOT NULL,
-    PRIMARY KEY (artist_id, track_id)
+    PRIMARY KEY (artist_id, track_id),
+    CONSTRAINT fk_track_id FOREIGN KEY (track_id) REFERENCES track(id),
+    CONSTRAINT fk_artist_id FOREIGN KEY (artist_id) REFERENCES user(id)
 );
 
--- A release is a collection of tracks presented as a group (e.g. an album)
-CREATE TABLE `release` (
+-- A collection of tracks presented as a group (e.g. an album)
+CREATE TABLE release_unit (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     type ENUM('album', 'single', 'ep', 'compilation') NOT NULL,
@@ -49,19 +52,19 @@ CREATE TABLE `release` (
     removed_on TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE release_track (
-    release_id INT NOT NULL,
+CREATE TABLE release_unit_track (
+    release_unit_id INT NOT NULL,
     track_id INT NOT NULL,
     order_num INT NOT NULL,
     disc_num INT NULL,
-    PRIMARY KEY (release_id, track_id)
+    PRIMARY KEY (release_unit_id, track_id)
 );
 
 -- Release artists are the artists that are credited on the release but not 
 -- necessarily on the tracks contained within the release
-CREATE TABLE release_artist (
-    release_id INT NOT NULL,
+CREATE TABLE release_unit_artist (
+    release_unit_id INT NOT NULL,
     artist_id INT NOT NULL,
-    PRIMARY KEY (release_id, artist_id)
+    PRIMARY KEY (release_unit_id, artist_id)
 );
 
