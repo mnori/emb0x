790f9e81fa5d2f1e63fee9f5c717059329dc9cad
Matthew Norris <matthew.gs.norris@gmail.com>
2025-03-30 04:20:45 +0100

    Better schema

--- bin/init-database.sql
+++ bin/init-database.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS track;
-DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS track_artist;
-DROP TABLE IF EXISTS release_unit;
-DROP TABLE IF EXISTS release_unit_track;
-DROP TABLE IF EXISTS release_unit_artist;
+DROP TABLE IF EXISTS tracklist_track;
+DROP TABLE IF EXISTS track;
+DROP TABLE IF EXISTS tracklist;
+DROP TABLE IF EXISTS tracklist_artist;
+DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT,
@@ -28,7 +28,7 @@ CREATE TABLE track (
     created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_on TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     removed_on TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_user_id FOREIGN KEY (artist_user_id) REFERENCES user(id),
+    FOREIGN KEY (artist_user_id) REFERENCES user(id),
     PRIMARY KEY (id)
 );
 
@@ -36,12 +36,12 @@ CREATE TABLE track_artist (
     track_id INT NOT NULL,
     artist_id INT NOT NULL,
     PRIMARY KEY (artist_id, track_id),
-    CONSTRAINT fk_track_id FOREIGN KEY (track_id) REFERENCES track(id),
-    CONSTRAINT fk_artist_id FOREIGN KEY (artist_id) REFERENCES user(id)
+    FOREIGN KEY (track_id) REFERENCES track(id),
+    FOREIGN KEY (artist_id) REFERENCES user(id)
 );
 
 -- A collection of tracks presented as a group (e.g. an album)
-CREATE TABLE release_unit (
+CREATE TABLE tracklist (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     type ENUM('album', 'single', 'ep', 'compilation') NOT NULL,
@@ -52,19 +52,23 @@ CREATE TABLE release_unit (
     removed_on TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE release_unit_track (
-    release_unit_id INT NOT NULL,
+CREATE TABLE tracklist_track (
+    tracklist_id INT NOT NULL,
     track_id INT NOT NULL,
     order_num INT NOT NULL,
     disc_num INT NULL,
-    PRIMARY KEY (release_unit_id, track_id)
+    PRIMARY KEY (tracklist_id, track_id),
+    FOREIGN KEY (tracklist_id) REFERENCES tracklist(id),
+    FOREIGN KEY (track_id) REFERENCES track(id)
 );
 
 -- Release artists are the artists that are credited on the release but not 
 -- necessarily on the tracks contained within the release
-CREATE TABLE release_unit_artist (
-    release_unit_id INT NOT NULL,
+CREATE TABLE tracklist_artist (
+    tracklist_id INT NOT NULL,
     artist_id INT NOT NULL,
-    PRIMARY KEY (release_unit_id, artist_id)
+    PRIMARY KEY (tracklist_id, artist_id),
+    FOREIGN KEY (tracklist_id) REFERENCES tracklist(id),
+    FOREIGN KEY (artist_id) REFERENCES user(id)
 );
 
