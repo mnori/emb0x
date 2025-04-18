40879be2339282064b6b01313c5d485e0bf5d612
Matthew Norris <matthew.gs.norris@gmail.com>
2025-04-18 12:43:02 +0100

    Better commit for github

--- webapp/obj/Debug/net9.0/webapp.AssemblyInfo.cs
+++ webapp/obj/Debug/net9.0/webapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2c7b5d3e7e39b234eaa066601c0845b6aa3f13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4fcf4ef102b950b040f2a668c6372d6233a6e28")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
