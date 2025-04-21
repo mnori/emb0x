0f9e26c4cc6cfb5b59afed955e30389e841e5c57
Matthew Norris <matthew.gs.norris@gmail.com>
2025-04-21 06:34:51 +0100

    Successfully dockerised the webapp

--- webapp/obj/Debug/net9.0/webapp.AssemblyInfo.cs
+++ webapp/obj/Debug/net9.0/webapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74d7b8ab490b30b6015e83e8bb0aa09812b0c21b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215c71c1e79a8ee3e924a4c64c59827a54014354")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
