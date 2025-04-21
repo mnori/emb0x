ec50fa4dc70901cdbb5ddcf8a058a3413d330a0a
Matthew Norris <matthew.gs.norris@gmail.com>
2025-04-21 07:59:01 +0100

    Attempts to dockerise MySQL and make it work with webapp

--- webapp/obj/Debug/net9.0/webapp.AssemblyInfo.cs
+++ webapp/obj/Debug/net9.0/webapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215c71c1e79a8ee3e924a4c64c59827a54014354")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c53a7ee4e6c39b656218db640b2d20c5a5c974")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
