05ea4b738087ca66c0e6bb415df3556f59a833fa
Matthew Norris <matthew.gs.norris@gmail.com>
2025-04-25 19:30:14 +0100

    Successful dockerisation of .net web server and mysql with both interacting

---
+++ docker-mysql/Program.cs
@@ -0,0 +1,27 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using MySql.Data.MySqlClient;
+
+namespace DockerMySQL {
+    class Program {
+        static void Main(string[] args) {
+            string connectionString = "server=database;user=root;password=test123;database=mysql";
+
+            System.Threading.Thread.Sleep(5000);
+
+            try {
+                using (MySqlConnection connection = new MySqlConnection(connectionString)) {
+                    connection.Open();
+                    connection.Close();
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine("Connected to MySQL!");
+        }
+    }
+}
--- webapp/obj/Debug/net9.0/webapp.AssemblyInfo.cs
+++ webapp/obj/Debug/net9.0/webapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c53a7ee4e6c39b656218db640b2d20c5a5c974")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec50fa4dc70901cdbb5ddcf8a058a3413d330a0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
