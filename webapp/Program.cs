93cd3a3ef7c04a1344a1c6dacd906e62c2c397ff
Matthew Norris <matthew.gs.norris@gmail.com>
2025-04-25 23:23:40 +0100

    major trouble getting webapp and mysql database docker container to play nicely

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -1,26 +1,52 @@
+using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
 using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 
-// this is version of Pomelo.EntityFrameworkCore.MySql
-var connectionString = builder.Configuration.GetConnectionString("MvcMovieContext");
-var mysqlVersion = ServerVersion.AutoDetect(connectionString);
+var projectRoot = Path.GetFullPath(AppContext.BaseDirectory);
+Directory.SetCurrentDirectory(projectRoot);
 
+System.Diagnostics.Debug.WriteLine($"## Current Directory: {Directory.GetCurrentDirectory()}");
+
+// this is version of Pomelo.EntityFrameworkCore.MySql
+// var connectionString = builder.Configuration.GetConnectionString("ConnectionStrings__MvcMovieContext");
+var connectionString = builder.Configuration.GetConnectionString("MvcMovieContext") ??
+                       Environment.GetEnvironmentVariable("ConnectionStrings__MvcMovieContext");
+System.Diagnostics.Debug.WriteLine($"## ConnectionString {connectionString}");
 if (builder.Environment.IsDevelopment())
 {
+    System.Diagnostics.Debug.WriteLine("## DEVELOPMENT ENVIRONMENT");
+    if (string.IsNullOrEmpty(connectionString))
+    {
+        throw new InvalidOperationException("The connection string 'MvcMovieContext' is not configured.");
+    }
+
     builder.Services.AddDbContext<MvcMovieContext>(options =>
         options.UseMySql(
             connectionString,
-            mysqlVersion // Specify the MySQL version
-        )
-    );
+            new MySqlServerVersion(new Version(8, 0, 32)) // Adjust MySQL version as needed
+        ));
+
+    // builder.Services.AddDbContext<MvcMovieContext>(options =>
+    // options.UseMySql(
+    //     connectionString,
+    //     new MySqlServerVersion(new Version(8, 0, 32)) // Adjust MySQL version as needed
+    // ));
+    
+    // builder.Services.AddDbContext<MvcMovieContext>(options =>
+    //     options.UseMySql(
+    //         connectionString,
+    //         mysqlVersion // Specify the MySQL version
+    //     )
+    // );
     // builder.Services.AddDbContext<MvcMovieContext>(options =>
     //     options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext")));
 }
 else
 {
+    System.Diagnostics.Debug.WriteLine("## NOT DEVELOPMENT ENVIRONMENT");
     builder.Services.AddDbContext<MvcMovieContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMvcMovieContext")));
 }
@@ -37,8 +63,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-
-    SeedData.Initialize(services);
+    // SeedData.Initialize(services); // disable for now
 }
 
 // Configure the HTTP request pipeline.
