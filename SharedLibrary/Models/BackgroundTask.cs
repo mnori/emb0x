9fb1a3ee07f18319dadd34a3f19906fbda132587
Matthew Norris <matthew.gs.norris@gmail.com>
2025-04-26 05:47:47 +0100

    More edits

---
+++ SharedLibrary/Models/BackgroundTask.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SharedLibrary.Models;
+
+public class BackgroundTask
+{
+    public string Id { get; set; } // UUID - matches the upload filename on the disk
+    public string Type { get; set; } // e.g. IMPORT
+    
+    public DateTime Created { get; set; }
+    public DateTime? Started { get; set; }
+    public DateTime? Completed { get; set; }
+    
+}
