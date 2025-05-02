cedddaef9108dbd92ca1ac6b99bb63597a0748ae
Matthew Norris <matthew.gs.norris@gmail.com>
2025-05-03 00:28:40 +0100

    Got the basics of a daemon working for import-manager

--- import-manager/Program.cs
+++ import-manager/Program.cs
@@ -1,11 +1,37 @@
 using Microsoft.EntityFrameworkCore;
 using SharedLibrary.Data;
 
-// var connectionString = builder.Configuration.GetConnectionString("TaskDatabase") ??
-//                        Environment.GetEnvironmentVariable("ConnectionStrings__TaskDatabase");
+namespace ImportManager
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-// builder.Services.AddDbContext<MvcMovieContext>(options =>
-//     options.UseMySql(
-//         connectionString,
-//         new MySqlServerVersion(new Version(8, 0, 32))
-//     ));
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Add DbContext
+                    services.AddDbContext<Emb0xDatabaseContext>();
+
+                    // Add your background service or other dependencies here
+                    services.AddHostedService<ImportTaskDaemon>();
+                });
+    }
+}
+
+
+// using Microsoft.EntityFrameworkCore;
+// using SharedLibrary.Data;
+
+// // var connectionString = builder.Configuration.GetConnectionString("TaskDatabase") ??
+// //                        Environment.GetEnvironmentVariable("ConnectionStrings__TaskDatabase");
+
+// // builder.Services.AddDbContext<MvcMovieContext>(options =>
+// //     options.UseMySql(
+// //         connectionString,
+// //         new MySqlServerVersion(new Version(8, 0, 32))
+// //     ));
