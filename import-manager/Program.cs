86e9003c635a26fce3210c867882730ce00fae63
Matthew Norris <matthew.gs.norris@gmail.com>
2025-05-03 01:13:34 +0100

    Dockerisation of daemon process

--- import-manager/Program.cs
+++ import-manager/Program.cs
@@ -22,16 +22,3 @@ namespace ImportManager
                 });
     }
 }
-
-
-// using Microsoft.EntityFrameworkCore;
-// using SharedLibrary.Data;
-
-// // var connectionString = builder.Configuration.GetConnectionString("TaskDatabase") ??
-// //                        Environment.GetEnvironmentVariable("ConnectionStrings__TaskDatabase");
-
-// // builder.Services.AddDbContext<MvcMovieContext>(options =>
-// //     options.UseMySql(
-// //         connectionString,
-// //         new MySqlServerVersion(new Version(8, 0, 32))
-// //     ));
