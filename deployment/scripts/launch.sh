490e5374f2ca5af7d6bad7fabffa9d5a7f117262
Matthew Norris <matthew.gs.norris@gmail.com>
2025-11-16 05:13:00 +0000

    Tidy up / comments

--- deployment/scripts/launch.sh
+++ deployment/scripts/launch.sh
@@ -2,16 +2,18 @@
 
 echo "Getting the party started..."
 source ./secrets.env
+
+# Teardown any existing resources because otherwise Bezos will ask for a pound of flesh
 ./teardown-ec2.sh
 ./teardown-eni.sh
 ./teardown-security-groups.sh
 ./teardown-subnet.sh
 ./teardown-vpc.sh
 
-# ./teardown.sh
-
+# Create the new versions of the stuff
 ./create-vpc.sh
 ./create-subnet.sh
 ./create-security-groups.sh
 ./create-ec2.sh
+
 echo "Deployment process reached end."
