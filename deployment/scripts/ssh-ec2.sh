9dc7e5264965208f2c11d672088d173d640e4618
Matthew Norris <matthew.gs.norris@gmail.com>
2025-11-22 15:01:27 +0000

    Tidy up

--- deployment/scripts/ssh-ec2.sh
+++ deployment/scripts/ssh-ec2.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# Doesn't work
+
 source ./secrets.env
 echo "SSHing into EC2 instance..."
 PUBLIC_IP=$(cat data/public-ip.txt)
 ssh-keygen -R $PUBLIC_IP
 ssh-keyscan -H -t ed25519,rsa,ecdsa $PUBLIC_IP >> ~/.ssh/known_hosts
 ssh -i data/emb0x-key.pem ubuntu@$PUBLIC_IP
-
-# ssh -o StrictHostKeyChecking=accept-new -i data/emb0x-key.pem ubuntu@$(cat data/public-ip.txt)
-# ./replace-key.sh
